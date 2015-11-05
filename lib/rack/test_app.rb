15555ba84118ac0d89d9266cf9380414217886df
makoto kuwata <kwa@kuwata-lab.com>
2015-11-05 23:08:38 +0900

    remove files under 'lib'

--- lib/rack/test_app.rb
+++
@@ -1,7 +0,0 @@
-require "rack/test_app/version"
-
-module Rack
-  module TestApp
-    # Your code goes here...
-  end
-end
--- lib/rack/test_app/version.rb
+++
@@ -1,5 +0,0 @@
-module Rack
-  module TestApp
-    VERSION = "0.1.0"
-  end
-end
